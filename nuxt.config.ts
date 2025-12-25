480cf225cd2f4f2b7203e393e96e3f40158eade0
schir <schir2@gmail.com>
2025-12-25 16:32:38 -0500

    Add project setup with key configurations and dependencies (Nuxt modules, Supabase, TailwindCSS, runtime config, app settings)

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,33 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+    compatibilityDate: '2025-07-15',
+    devtools: {enabled: true},
+    modules: ['@nuxt/icon', '@nuxtjs/supabase', '@nuxtjs/tailwindcss'],
+
+    app: {
+        head: {
+            title: 'mschir.dev',
+            htmlAttrs: {
+                lang: 'en',
+                class: 'dark-mode',
+            }
+        }
+    },
+
+    runtimeConfig: {
+        public: {
+            appName: 'Marek Schir  Portfolio',
+            defaultTitle: 'Marek Schir Developer Portfolio Site',
+            siteUrl: process.env.SITE_URL || 'http://localhost:3000',
+            port: parseInt(process.env.PORT || '3000'),
+        }
+    },
+
+    supabase: {
+        redirectOptions: {
+            login: '/login',
+            exclude: ['/', '/register', '/docs', '/features', '/faq', '/callback', '/project-invites/**'],
+            callback: '/callback'
+        }
+    },
 })
