a96dd0d140763aabdc73345a19b3361df1b4a791
schir <schir2@gmail.com>
2026-05-27 00:51:30 -0400

    Remove unused logic and references in `callback.vue`.

--- app/pages/(auth)/callback.vue
+++ app/pages/(auth)/callback.vue
@@ -1,16 +1,6 @@
 <script lang="ts" setup>
-const {userProfile, load} = useUserProfileService()
-onMounted(async () => {
-  await load()
-  if (!userProfile?.value) {
-    navigateTo('/create_profile')
-  }
-  else{
-    navigateTo('/')
-  }
-})
+
 </script>
 <template>
-  {{userProfile}}
   <h1>Callback</h1>
 </template>
