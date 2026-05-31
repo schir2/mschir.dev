561c4b6abbe4719d354ea1565bbd6692d0305470
schir <schir2@gmail.com>
2026-05-30 20:40:41 -0400

    Refactor `Timeline` component: add skill icons, improve project skills rendering, and format code.

--- app/components/project/Timeline.vue
+++ app/components/project/Timeline.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import type { ProjectWithSkills } from '#shared/types/Projects'
+import type {ProjectWithSkills} from '#shared/types/Projects'
 
 defineProps<{
   projects: ProjectWithSkills[]
@@ -12,7 +12,7 @@ defineProps<{
     <p-timeline :value="projects" align="alternate" class="customized-timeline">
       <template #marker>
         <span class="flex w-8 h-8 items-center justify-center text-white rounded-full z-10 shadow-sm bg-primary">
-          <Icon name="material-symbols:code" />
+          <Icon name="material-symbols:code"/>
         </span>
       </template>
       <template #content="slotProps">
@@ -20,14 +20,18 @@ defineProps<{
           <template #title>{{ slotProps.item.name }}</template>
           <template #subtitle>{{ slotProps.item.year }}</template>
           <template #content>
-            <img v-if="slotProps.item.image_url" :src="slotProps.item.image_url" :alt="slotProps.item.name" width="200" class="shadow-sm mb-2" />
+            <img v-if="slotProps.item.image_url" :src="slotProps.item.image_url" :alt="slotProps.item.name" width="200"
+                 class="shadow-sm mb-2"/>
             <p>{{ slotProps.item.description }}</p>
             <div v-if="slotProps.item.project_skills?.length" class="flex flex-wrap gap-1 mt-2">
-              <p-chip
-                v-for="{ skills } in slotProps.item.project_skills"
-                :key="skills.id"
-                :label="skills.name"
-              />
+              <template v-for="{ skills : skill } in slotProps.item.project_skills"
+                        :key="skill.id">
+                <p-chip :label="skill.name" >
+                  <template #icon>
+                    <icon v-if="skill.icon" :name="skill.icon"/>
+                  </template>
+                </p-chip>
+              </template>
             </div>
           </template>
         </p-card>
