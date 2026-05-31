90cf42e4534dff9ba81ccb559cdee9a65e94a7e0
schir <schir2@gmail.com>
2026-05-31 01:55:51 -0400

    Add `ArticleArchivedBanner` component to display archived notice in UI

---
+++ app/components/article/ArticleArchivedBanner.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+defineProps<{
+  archivedAt: string | null
+}>()
+</script>
+
+<template>
+  <div v-if="archivedAt" role="alert">
+    This article has been archived and may be outdated.
+  </div>
+</template>
