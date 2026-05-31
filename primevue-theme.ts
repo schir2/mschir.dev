ff6a859be800ac96a5bae6460c6f03aa7af62d0e
schir <schir2@gmail.com>
2026-05-31 13:40:15 -0400

    Extend `primevue-theme` with accent color palette using amber shades

--- primevue-theme.ts
+++ primevue-theme.ts
@@ -29,5 +29,18 @@ export const customTheme = definePreset(Aura, {
             900: '{emerald.900}',
             950: '{emerald.950}'
         },
+        accent: {
+            50: '{amber.50}',
+            100: '{amber.100}',
+            200: '{amber.200}',
+            300: '{amber.300}',
+            400: '{amber.400}',
+            500: '{amber.500}',
+            600: '{amber.600}',
+            700: '{amber.700}',
+            800: '{amber.800}',
+            900: '{amber.900}',
+            950: '{amber.950}'
+        },
     }
 })
