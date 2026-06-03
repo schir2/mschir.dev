8ebeb1d0a9625daece19b2c11ee3f7dde796a285
schir <schir2@gmail.com>
2026-06-03 00:15:43 -0400

    chore: add `FormField` to auto-imports in Nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -84,6 +84,7 @@ export default defineNuxtConfig({
                 'RadioButton',
                 'RadioGroup',
                 'Form',
+                'FormField',
                 'Password',
                 'Button',
                 'Toast',
